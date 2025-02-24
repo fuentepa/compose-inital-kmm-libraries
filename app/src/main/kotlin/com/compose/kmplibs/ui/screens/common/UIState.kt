874107700ed0c78eaffb4cf9eb86fb2960233940
Pablo Antonio Fuente <pafuente@paradigmadigital.com>
2025-02-24 09:34:58 +0100

    update ktor and ktorfit libraries

--- app/src/main/kotlin/com/compose/kmplibs/ui/screens/common/UIState.kt
+++ app/src/main/kotlin/com/compose/kmplibs/ui/screens/common/UIState.kt
@@ -1,7 +1,7 @@
 package com.compose.kmplibs.ui.screens.common
 
-sealed class UIState<out T> {
-    class Loading<T> : UIState<T>()
-    data class Success<T>(val data: T) : UIState<T>()
-    data class Error<T>(val error: String) : UIState<T>()
+sealed interface UIState<out T> {
+    class Loading<T> : UIState<T>
+    data class Success<T>(val data: T) : UIState<T>
+    data class Error<T>(val error: String) : UIState<T>
 }
